d39fcc1286041a7988a649f31fcc9be2330f89f1
gfbwff <476263617@qq.com>
2017-01-11 09:36:50 +0800

    app

--- littleAlbum/app.js
+++ littleAlbum/app.js
@@ -1,3 +1,4 @@
+
 /**
  * Created by ganfengbao on 2017/1/10.
  */
@@ -6,9 +7,16 @@ var app = express();
 var router = require('./controller');
 
 app.use(express.static('./public'));
+app.use(express.static('./uploads'));
 app.set('view engine','ejs');
 
 app.get('/',router.showIndex);
 app.get('/:albumName',router.showAlbum);
+
+app.use(function(req,res){
+    res.render('err',{
+        "baseurl":req.pathname
+    });
+});
 
 app.listen(3000);
